012c76984a74981999b80bd8f1e57344acd77fce
Karim Essam <kaessam17@gmail.com>
2024-02-12 01:27:05 +0200

    ASP.Net MVC Day 1

---
+++ ASP.Net MVC/ASP.Net MVC Day1/ASP.Net MVC Day1/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP.Net_MVC_Day1.Models;
+
+namespace ASP.Net_MVC_Day1.Controllers
+{
+    public class EmployeeController: Controller
+    {
+        public string Welcome()
+        {
+            return "Welcome";
+        }
+
+        public JsonResult All()
+        {
+            return Json(CompanyDBContext.Employees, JsonRequestBehavior.AllowGet);
+        }
+
+        public RedirectToRouteResult add(int id, string name)
+        {
+            CompanyDBContext.Employees.Add(new Employee() { ID = id, Name = name });
+            return RedirectToAction("All");
+        }
+
+        public RedirectToRouteResult Update(int id, string newName)
+        {
+            var exictingEmployee = CompanyDBContext.Employees.FirstOrDefault(e => e.ID == id);
+
+                exictingEmployee.Name = newName;
+
+                return RedirectToAction("All");
+        }
+
+        public RedirectToRouteResult Remove(int id)
+        {
+            var EmployeeToRemove = CompanyDBContext.Employees.FirstOrDefault(e => e.ID == id);
+
+            
+                CompanyDBContext.Employees.Remove(EmployeeToRemove);
+
+            return RedirectToAction("All");
+        }
+    }
+}
---
+++ ASP.Net MVC/ASP.Net MVC Day1/ASP.Net MVC Day1/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.Net_MVC_Day1.Models
+{
+    public class CompanyDBContext
+    {
+        public static List<Employee> Employees = new List<Employee>()
+        {
+            new Employee() { ID = 1, Name = "Karim" },
+            new Employee() { ID = 2, Name = "Essam" }
+
+        };
+    }
+}
